34660cae5cd709a528b2aca944d3c3a84fb5750a
Juillian Lee <juillian.lee@gmail.com>
2022-01-15 18:01:26 -0300

    feat: Adicionado melhorias de TDD.

---
+++ src/infra/http/controller/account/create_test.go
@@ -0,0 +1,60 @@
+package controller
+
+import (
+	usecase "app-helley/src/app/usecase/account"
+	"app-helley/src/infra/http/dto"
+	"app-helley/src/infra/http/setup"
+	repository "app-helley/src/infra/repository/memory"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateAccountSuccessfully(t *testing.T) {
+
+	userRepository := &repository.UserRepository{}
+
+	usecase := usecase.NewCreateAccountUseCase(userRepository)
+
+	e := setup.SetupRouter()
+	req := httptest.NewRequest(http.MethodPost, "/sign-up", strings.NewReader(`{"name": "Juillian Lee", "email": "juillian.lee@gmail.com", "password": "acb123"}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	err := NewCreateAccountController(usecase).Handle(c)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, rec.Code)
+
+	var response dto.UserResponse
+	json.Unmarshal(rec.Body.Bytes(), &response)
+	assert.NotEmpty(t, response.ID)
+	assert.NotEmpty(t, response.Name)
+	assert.NotEmpty(t, response.Email)
+}
+
+func TestCreateAccountBadRequestEmptyBody(t *testing.T) {
+	userRepository := &repository.UserRepository{}
+
+	usecase := usecase.NewCreateAccountUseCase(userRepository)
+
+	req := httptest.NewRequest(http.MethodPost, "/sign-up", strings.NewReader(`{}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	e := setup.SetupRouter()
+	c := e.NewContext(req, rec)
+
+	err := NewCreateAccountController(usecase).Handle(c)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusBadRequest, rec.Code)
+
+	var response dto.ErrorResponse
+	json.Unmarshal(rec.Body.Bytes(), &response)
+	assert.Equal(t, response.StatusCode, http.StatusBadRequest)
+	assert.NotEmpty(t, response.Message)
+}
